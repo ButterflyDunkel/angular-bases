b934494ee184931510718840515010cf1ca7464f
HellenRoxely <roxely_garcia@hotmail.com>
2023-11-10 12:26:22 -0600

    ngClass, FormModule, ngModel y Output

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 
 export class MainPageComponent {
 
+  public characters: Character[] = [{
+    name: 'Krillin',
+    power: 1000
+  }, {
+    name: 'Goku',
+    power: 9500
+  },{
+    name: 'Vegeta',
+    power: 7500
+  }];
+
+  onNewCharacter(character:Character):void{
+    console.log('MainPage');
+    console.log(character);
+  }
 }
